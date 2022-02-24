c1dafcf9659eed97d10711cb28e0cc719bda47a6
Anna <Laluna-93@yandex.ru>
2022-02-24 18:26:52 +0400

    init: start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const obj = Object.fromEntries(bracketsConfig)
+    console.log(obj)
+    const left = ['(', '{' ,'[', '1', '3', '5', '7', '8', '|']
+    let arr =[]
+    for(let i = 0; i<str.length; i++){
+    let simbol = str[i]
+     if(obj[arr[arr.length-1]]===simbol){
+     arr.pop()
+        }else if(left.includes(simbol)){
+            arr.push(simbol)
+            }  else {
+                if(arr.length ===0){
+                return false
+        }
+        }
+    }
+    return arr.length===0
 }
